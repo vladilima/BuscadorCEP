c63df0aa0d2f861063651a0ea7ecc972698a40c9
vladilima <vladilima1001@gmail.com>
2025-06-02 17:30:25 -0300

    feat: tipo Endereco

---
+++ src/Endereco.java
@@ -0,0 +1,17 @@
+public class Endereco {
+
+    String CEP;
+    String estado;
+    String localidade;
+    String bairro;
+    String logradouro;
+
+    public Endereco(String CEP, String estado, String localidade, String bairro, String logradouro) {
+        this.CEP = CEP;
+        this.estado = estado;
+        this.localidade = localidade;
+        this.bairro = bairro;
+        this.logradouro = logradouro;
+    }
+
+}
