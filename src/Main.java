06f91f1dded22aad5304316736187bf8287e1546
vladilima <vladilima1001@gmail.com>
2025-06-02 17:18:45 -0300

    Feat: busca básica na API

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um CEP para busca: ");
+        var CEP = input.nextLine();
+
+        String endereco = "https://viacep.com.br/ws/" + CEP + "/json/";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
     }
 }
